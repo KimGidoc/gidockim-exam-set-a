3710e28cbbf80592b0057ccbec470133ae44bdd1
gidockimberly <gidockimberly@gmail.com>
2023-01-09 21:13:59 +0800

    init

---
+++ exam.js
@@ -0,0 +1,25 @@
+const read = require('./read')
+const employee = require('./employee')
+const add = require('./add')
+const present = require('./present')
+
+const data = process.argv
+
+var note = {}
+if (data[2] == 'add'){
+    note = {
+        employee: data [3],
+        role: data[4],
+        department: data[5]
+    }
+    var oldNote = read()
+    add (note, oldNote)
+
+}
+if (data [2] == 'read'){
+    present(read())
+}
+if (data [2] == 'employee'){
+    employee(read())
+}
+
